71a2791c3f0e2b571848f9465c65ec93430e188c
IMBABOT1 <zlovred12@gmail.com>
2021-01-28 13:07:13 +0300

    overloading

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,25 @@
 package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private int add(int a, int b){
+        return a + b;
+    }
 
+    private float add(float a, float b, float c){
+        return a + b + c;
+    }
+
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        System.out.println(main.add(1, 2));
+        System.out.println(main.add(2,3,4.5f));
     }
 }
 
